2bcf10e289540c7185204cda3a848e7ec673c5f5
sjaymoon15 <sjaymoon15@gmail.com>
2018-07-07 16:32:43 -0700

    Register Service worker and cache assets

---
+++ sw.js
@@ -0,0 +1,53 @@
+let staticCacheName = 'mwnd-v2';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/sw.js',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/register.js',
+        '/js/restaurant_info.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('mwnd-') &&
+            cacheName != staticCacheName;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }));
+});
